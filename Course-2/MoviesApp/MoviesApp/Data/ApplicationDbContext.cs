c78b03fc06cf3609b3be101b1aa6dcd3d6764796
CalebKing848 <calebking848@outlook.com>
2023-10-30 06:57:01 +1300

    Setting up Entity Framework Core in ASP.NET Razor Pages

---
+++ Course-2/MoviesApp/MoviesApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesApp.Data.Models;
+
+namespace MoviesApp.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+    }
+}
