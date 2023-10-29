b95749a1c5e163d87d366735faf5e046d0985a36
CalebKing848 <calebking848@outlook.com>
2023-10-29 19:23:06 +1300

    Added Test to Application

---
+++ Course-2/MoviesApp/MoviesApp/Pages/Test.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MoviesApp.Pages
+{
+    public class TestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
