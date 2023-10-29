737d75c877f590adc50f07ad7841dc91029484a7
CalebKing848 <calebking848@outlook.com>
2023-10-29 20:04:44 +1300

    Added Two-way data binding (Add new Movies)

--- Course-2/MoviesApp/MoviesApp/Pages/AddMovie.cshtml.cs
+++ Course-2/MoviesApp/MoviesApp/Pages/AddMovie.cshtml.cs
@@ -5,13 +5,25 @@ namespace MoviesApp.Pages
 {
     public class AddMovieModel : PageModel
     {
+        [BindProperty]
         public string Title { get; set; }
+
+        [BindProperty]
         public int Rate {  get; set; }
-        
+
+        [BindProperty]
         public string Description { get; set; }
 
         public void OnGet()
         {
+            Title = "Welcome";
+        }
+
+        public IActionResult OnPost()
+        {
+            string value = $"{Title} - {Rate} - {Description}";
+            return Page();
+            //return Redirect("Movies");
         }
     }
 }
