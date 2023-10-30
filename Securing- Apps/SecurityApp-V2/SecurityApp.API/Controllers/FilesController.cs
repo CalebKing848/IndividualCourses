1236d36764a30821d88cf8e133c35137ee6a59bb
CalebKing848 <calebking848@outlook.com>
2023-10-30 15:52:57 +1300

    Renamed File and removed code which was accedently added

--- Securing- Apps/SecurityApp-V2/SecurityApp.API/Controllers/FilesController.cs
+++ Securing- Apps/SecurityApp-V2/SecurityApp.API/Controllers/FilesController.cs
@@ -9,18 +9,9 @@ namespace SecurityApp.API.Controllers
     public class FilesController : ControllerBase
     {
         [HttpPost]
-        //[Authorize]
-        public async Task<IActionResult> Upload([FileExtensions(Extensions =".pdf,.txt")][MaxLength(10*1024)]IFormFile file)
+        public async Task<IActionResult> Upload(IFormFile file)
         {
-            ////Check File Extension
-            //var extensions = Path.GetExtension(file.FileName);
-            //if (extensions != ".pdf") return BadRequest("Only .pdf files are allowed");
 
-            ////Check File Size
-            //if(file.Length > 10 * 1024) return BadRequest($"File is larger than 10kb");
-
-            //Generate a Random Name
-            
             // Save the uploaded file to a temporary location
             var path = Path.GetTempFileName();
             using (var stream = System.IO.File.Create(path))
