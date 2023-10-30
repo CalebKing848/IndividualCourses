4e9894a6e3735ea82c4d82b79ed2184b94c5d509
CalebKing848 <calebking848@outlook.com>
2023-10-31 09:01:58 +1300

    XUnit Theory with ClassData

--- Unit-Testing/CalculatorApp/CalculatorApp/MathHelper.cs
+++ Unit-Testing/CalculatorApp/CalculatorApp/MathHelper.cs
@@ -41,10 +41,8 @@ namespace CalculatorApp
 
         public IEnumerator<object[]> GetEnumerator()
         {
-            yield return new object[] { 1, 2, 3 };
+            yield return new object[] { 1, 2, 4 };
             yield return new object[] { -4, -6, -10 };
-            yield return new object[] { -2, 2, 0 };
-            yield return new object[] { int.MinValue, -1, int.MaxValue };
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
