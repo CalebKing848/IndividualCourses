94f0ea071821abc93eaade07938d5c4fbc95e578
CalebKing848 <calebking848@outlook.com>
2023-10-31 17:00:43 +1300

    Increase maintainability with dependency injection

--- Learning MVC/ExploreCalifornia/ExploreCalifornia/Startup.cs
+++ Learning MVC/ExploreCalifornia/ExploreCalifornia/Startup.cs
@@ -23,15 +23,22 @@ namespace ExploreCalifornia
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<FeatureToggles>(x => new FeatureToggles
+            {
+                DeveloperExceptions = configuration.GetValue<bool>("FeatureToggles:DeveloperExceptions")
+            }); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(
+            IApplicationBuilder app,
+            IHostingEnvironment env,
+            FeatureToggles features)
         {
 
             app.UseExceptionHandler("/error.html");
 
-            if (configuration.GetValue<bool>("FeatureToggles:DeveloperExceptions"))
+            if (features.DeveloperExceptions)
             {
                 app.UseDeveloperExceptionPage();
             }
