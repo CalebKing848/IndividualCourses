eb2f2b143028dc1aba4ce097312de1b3e061bc63
CalebKing848 <calebking848@outlook.com>
2023-11-01 13:22:58 +1300

    Had to fix Blog controller as blogs were not saving in database

--- Learning MVC/ExploreCalifornia/ExploreCalifornia/Controllers/BlogController.cs
+++ Learning MVC/ExploreCalifornia/ExploreCalifornia/Controllers/BlogController.cs
@@ -1,19 +1,46 @@
-﻿using ExploreCalifornia.Models;
-using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ExploreCalifornia.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ExploreCalifornia.Controllers
 {
     [Route("blog")]
     public class BlogController : Controller
     {
+        private readonly BlogDataContext _db;
+
+        public BlogController(BlogDataContext db)
+        {
+            _db = db;
+        }
+
         [Route("")]
         public IActionResult Index()
         {
-            return View();
+            var posts = new[]
+            {
+                new Post
+                {
+                    Title = "My blog post",
+                    Posted = DateTime.Now,
+                    Author = "Jess Chadwick",
+                    Body = "This is a great blog post, don't you think?",
+                },
+                new Post
+                {
+                    Title = "My second blog post",
+                    Posted = DateTime.Now,
+                    Author = "Jess Chadwick",
+                    Body = "This is ANOTHER great blog post, don't you think?",
+                },
+            };
+
+            return View(posts);
         }
 
         [Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
@@ -21,15 +48,15 @@ namespace ExploreCalifornia.Controllers
         {
             var post = new Post
             {
-               Title = "My blog post",
-               Posted = DateTime.Now,
-               Author = "Jess Chadwick",
-               Body = "This is a great blog post, don't you think?",
+                Title = "My blog post",
+                Posted = DateTime.Now,
+                Author = "Jess Chadwick",
+                Body = "This is a great blog post, don't you think?",
             };
-            
+
             return View(post);
         }
-    
+
         [HttpGet, Route("create")]
         public IActionResult Create()
         {
@@ -39,14 +66,16 @@ namespace ExploreCalifornia.Controllers
         [HttpPost, Route("create")]
         public IActionResult Create(Post post)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
                 return View();
 
             post.Author = User.Identity.Name;
             post.Posted = DateTime.Now;
 
+            _db.Posts.Add(post);
+            _db.SaveChanges();
+
             return View();
         }
-
     }
 }
--- Learning MVC/ExploreCalifornia/ExploreCalifornia/Models/BlogDataContext.cs
+++ Learning MVC/ExploreCalifornia/ExploreCalifornia/Models/BlogDataContext.cs
@@ -8,7 +8,7 @@ namespace ExploreCalifornia.Models
 {
     public class BlogDataContext : DbContext
     {
-        public DbSet<Post> Post { get; set; }
+        public DbSet<Post> Posts { get; set; }
 
         public BlogDataContext(DbContextOptions<BlogDataContext> options)
                 :base(options)
